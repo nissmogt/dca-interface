b7d58063c6ec61d70f6bbb2c325c2b0be2c0e4b1
nissmogt <nissmogt@users.noreply.github.com>
2020-04-23 09:50:48 -0500

    added .cif support and heavy atom mindist

--- pdb_contacts.py
+++ pdb_contacts.py
@@ -7,8 +7,10 @@ Functions for calculating a PDB distance matrix.
     pdb_map(pdbfilename, ['Chain A', 'Chain B'], distance cutoff)
         Returns:
 """
+
+
 # dev branch test
-def get_residues(pdb_fn, chain_ids=None):
+def get_residues(pdbfile, chain_ids=None):
     import Bio.PDB
     import os
     import sys
@@ -20,11 +22,17 @@ def get_residues(pdb_fn, chain_ids=None):
         A list of Bio.PDB.Residue objects.
     """
 
-    pdb_id = os.path.splitext(os.path.basename(pdb_fn))[0]
-
-    parser = Bio.PDB.PDBParser(pdb_id, pdb_fn)
-    struct = parser.get_structure(pdb_id, pdb_fn)
-    model = struct[0]
+    pdb_id = os.path.splitext(os.path.basename(pdbfile))[0]
+    if os.path.basename(pdbfile).split('.')[-1] == 'pdb':
+        print('.pdb file...')
+        parser = Bio.PDB.PDBParser()
+        struct = parser.get_structure(pdb_id, pdbfile)
+        model = struct[0]
+    else:
+        print('.cif file...')
+        parser = Bio.PDB.MMCIFParser()
+        struct = parser.get_structure(pdb_id, pdbfile)
+        model = struct[0]
 
     if chain_ids is None:
         # get residues from every chain.
@@ -45,7 +53,28 @@ def get_residues(pdb_fn, chain_ids=None):
     return residues
 
 
-def calc_distance(res_a, res_b):
+def calc_ha_distance(res_a, res_b):
+    """Calculate the Euclidean distance between a pair of heavy atoms
+    Args:
+        res_a: A ``Bio.PDB.Residue`` object.
+        res_b: A ``Bio.PDB.Residue`` object.
+    Returns:
+        The distance between ``res_a`` and ``res_b``.
+    """
+    import numpy as np
+    min_dist = 8.0
+    for a in res_a.get_iterator():
+        for b in res_b.get_iterator():
+            coord_a = a.get_coord()
+            coord_b = b.get_coord()
+            dist = np.linalg.norm(coord_a - coord_b)
+            if dist < min_dist:
+                min_dist = dist
+
+    return min_dist
+
+
+def calc_ca_distance(res_a, res_b):
     import numpy as np
     """Calculate the Euclidean distance between a pair of residues
     according to a given distance metric.
@@ -55,41 +84,47 @@ def calc_distance(res_a, res_b):
     Returns:
         The distance between ``res_a`` and ``res_b``.
     """
-    # P1: add CB-CB distanct calculation (except for GLY)
-    A = res_a["CA"].get_coord()
-    B = res_b["CA"].get_coord()
-    dist = np.linalg.norm(A - B)
+    a = res_a["CA"].get_coord()
+    b = res_b["CA"].get_coord()
+    dist = np.linalg.norm(a - b)
     return dist
 
 
 def pdb_map(pdbfile, chain_ids, cutoff, ):
     import os
-    import numpy as np
     from pandas import read_csv
     from itertools import combinations_with_replacement
+    import time
+
     pdbname = os.path.splitext(pdbfile)[0]
-    #     cutoff = 10
-    # chain_ids = ['C','D']
     # output filename
     filename = pdbname + "_" + str(cutoff) + "A.txt"
     fileout = open(filename, 'w')
-    # create list of residues from pdb
-    residues = get_residues(pdbfile, chain_ids=chain_ids)
-    # make each possible pairs of residues
-    pair_indices = combinations_with_replacement(range(len(residues)), 2)
     # write header for output file
     fileout.write("i\tj\td\tchain_1\tchain_2\n")
+
+    # create list of residues from pdb
+    residues = get_residues(pdbfile, chain_ids=chain_ids)
+
+    # make each possible pairs of residues
+    pair_indices = combinations_with_replacement(range(len(residues)), 2)
+    start_time = time()
     for i, j in pair_indices:
         res_a = residues[i]
         res_b = residues[j]
         # get chain id
         chain_a = res_a.get_parent().id
         chain_b = res_b.get_parent().id
-        dist = calc_distance(res_a, res_b)
-        if cutoff >= dist > 0:
-            fileout.write("%d\t%d\t%f\t%s\t%s\n" % (i + 1, j + 1, dist, chain_a, chain_b))
+        dist = calc_ha_distance(res_a, res_b)
+    #    if cutoff >= dist > 0:
+        fileout.write("%d\t%d\t%f\t%s\t%s\n" % (i + 1, j + 1, dist, chain_a, chain_b))
+
+    print("Loop time: %s" % (time() - start_time))
     # makes a pandas dataframe
     df_pdb = read_csv(filename, delim_whitespace=True)
     df_mon = df_pdb[df_pdb['chain_1'] == df_pdb['chain_2']]
     df_inter = df_pdb[df_pdb['chain_1'] != df_pdb['chain_2']]
+
     return df_pdb, df_mon, df_inter
+
+
